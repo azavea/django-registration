bcc42d4e994f95d9bd3d7949ad9e924204e943c6
Justin Walgran <jwalgran@azavea.com>
2013-07-25 18:05:53 -0400

    Apply patch to work with custom user models
    Applied from
    https://bitbucket.org/prjts/django-registration/commits/ba31fc3053bfca7eb7a19d912882e50e295adc55
    
    Paul Walsh committed 2013-04-19
    Fixes *required* to make django-registration work with a custom user model
    that subclasses AbstractUser, in Django 1.5. This breaks Django 1.4
    compatibility in demostratiing how the current Django 1.5 compatibility
    of django-registration is flawed, when actually using the customer user
    model of Django 1.5

--- registration/models.py
+++ registration/models.py
@@ -4,7 +4,7 @@ import random
 import re
 
 from django.conf import settings
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.db import transaction
 from django.template.loader import render_to_string
@@ -81,7 +81,7 @@ class RegistrationManager(models.Manager):
         user. To disable this, pass ``send_email=False``.
         
         """
-        new_user = User.objects.create_user(username, email, password)
+        new_user = get_user_model().objects.create_user(username, email, password)
         new_user.is_active = False
         new_user.save()
 
@@ -158,7 +158,7 @@ class RegistrationManager(models.Manager):
                     if not user.is_active:
                         user.delete()
                         profile.delete()
-            except User.DoesNotExist:
+            except get_user_model().DoesNotExist:
                 profile.delete()
 
 class RegistrationProfile(models.Model):
@@ -179,7 +179,7 @@ class RegistrationProfile(models.Model):
     """
     ACTIVATED = u"ALREADY_ACTIVATED"
     
-    user = models.ForeignKey(User, unique=True, verbose_name=_('user'))
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, unique=True, verbose_name=_('user'))
     activation_key = models.CharField(_('activation key'), max_length=40)
     
     objects = RegistrationManager()
